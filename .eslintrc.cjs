ccae2c4e69d3ca40e465db613f8ad90ef245bfc1
Jochem Harteveld <jochemh83@gmail.com>
2025-10-13 15:03:00 +0200

    chore(dx): add ESLint, Prettier, Husky and lint-staged configs

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2024,
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  rules: {
+    'no-unused-vars': 'warn',
+    '@typescript-eslint/explicit-module-boundary-types': 'off'
+  }
+};
